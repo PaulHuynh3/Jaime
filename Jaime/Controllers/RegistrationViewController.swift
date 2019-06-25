5436d018a3c21cc2ec5244f0efbde3bc161b1a17
Paul Huynh <paul.huynh@aircanada.ca>
2019-06-25 00:21:22 -0400

    support landscape

--- Jaime/Controllers/RegistrationViewController.swift
+++ Jaime/Controllers/RegistrationViewController.swift
@@ -56,13 +56,25 @@ class RegistrationViewController: UIViewController {
         button.layer.cornerRadius = 22 //to make it round it should be half the height
         return button
     }()
-    //lazy var means stackview is created after buttons, textfield created
-    lazy var stackView = UIStackView(arrangedSubviews: [
-        selectPhotoButton,
+    fileprivate let gradientLayer = CAGradientLayer()
+
+    lazy var verticalStackView: UIStackView = {
+      let sv = UIStackView(arrangedSubviews: [
         nameTextField,
         emailTextField,
         passwordTextfield,
         registerButton
+        ])
+        sv.axis = .vertical
+        sv.distribution = .fillEqually
+        sv.spacing = 8
+        return sv
+    }()
+
+    //lazy var means stackview is created after buttons, textfield created
+    lazy var overallStackView = UIStackView(arrangedSubviews: [
+        selectPhotoButton,
+        verticalStackView
         ])
 
     override func viewDidLoad() {
@@ -75,6 +87,11 @@ class RegistrationViewController: UIViewController {
 
     override func viewWillDisappear(_ animated: Bool) {
         NotificationCenter.default.removeObserver(self) //prevent retain cycles
+    }
+
+    override func viewWillLayoutSubviews() { //basically called when view changes
+        super.viewWillLayoutSubviews()
+        gradientLayer.frame = view.bounds //expand the gradient to support landscape
     }
 
     // MARK:- Functions
@@ -97,7 +114,7 @@ class RegistrationViewController: UIViewController {
         guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         let keyboardFrame = value.cgRectValue
 
-        let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+        let bottomSpace = view.frame.height - overallStackView.frame.origin.y - overallStackView.frame.height
 
         let difference = keyboardFrame.height - bottomSpace
         self.view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
@@ -110,21 +127,26 @@ class RegistrationViewController: UIViewController {
     }
 
     fileprivate func setupGradientLayer() {
-        let gradientLayer = CAGradientLayer()
         let topColor = #colorLiteral(red: 0.9893777966, green: 0.347874403, blue: 0.382784009, alpha: 1)
         let bottomColor = #colorLiteral(red: 0.9005830884, green: 0.127312541, blue: 0.4597411752, alpha: 1)
         gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
         gradientLayer.locations = [0, 1]
         view.layer.addSublayer(gradientLayer)
-        gradientLayer.frame = view.bounds
+    }
+    //check if landscape vs portrait
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if self.traitCollection.verticalSizeClass == .compact {
+            overallStackView.axis = .horizontal
+        } else {
+            overallStackView.axis = .vertical
+        }
     }
 
     fileprivate func setupLayout() {
-        view.addSubview(stackView)
-        stackView.axis = .vertical
-        stackView.spacing = 8
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
-        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-
+        view.addSubview(overallStackView)
+        selectPhotoButton.widthAnchor.constraint(equalToConstant: 275).isActive = true
+        overallStackView.spacing = 8
+        overallStackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        overallStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
