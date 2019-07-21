78d4718b035cba018422dd10c2dab9049e35a12f
Paul Huynh <paul.huynh@aircanada.ca>
2019-07-21 17:50:43 -0400

    save user info to firebase

--- Jaime/Controllers/SettingsTableViewController.swift
+++ Jaime/Controllers/SettingsTableViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import JGProgressHUD
+import SDWebImage
 
 class CustomImagePickController: UIImagePickerController {
 
@@ -21,7 +24,18 @@ class SettingsTableViewController: UITableViewController, UIImagePickerControlle
     lazy var image2Button = createButton(selector: #selector(handleSelectPhoto))
     lazy var image3Button = createButton(selector: #selector(handleSelectPhoto))
 
-    func createButton(selector: Selector) -> UIButton {
+    var user: User?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationItems()
+        tableView.backgroundColor = UIColor(white: 0.96, alpha: 1)
+        tableView.tableFooterView = UIView()
+        tableView.keyboardDismissMode = .interactive //dragging page down keyboard goes down too
+        fetchCurrentUser()
+    }
+
+    fileprivate func createButton(selector: Selector) -> UIButton {
         let button = UIButton(type: .system)
         button.setTitle("Select Photo", for: .normal)
         button.backgroundColor = .white
@@ -47,12 +61,27 @@ class SettingsTableViewController: UITableViewController, UIImagePickerControlle
         dismiss(animated: true)
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupNavigationItems()
-        tableView.backgroundColor = UIColor(white: 0.96, alpha: 1)
-        tableView.tableFooterView = UIView()
-        tableView.keyboardDismissMode = .interactive //dragging page down keyboard goes down too
+    fileprivate func fetchCurrentUser() {
+       guard let uid = Auth.auth().currentUser?.uid else { return } //fetch specific user
+        Firestore.firestore().collection("users").document(uid).getDocument { (snapShot, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            //fetch user
+            guard let dictionary = snapShot?.data() else { return }
+            self.user = User(dictionary: dictionary)
+            self.loadUserPhotos()
+            self.tableView.reloadData()
+        }
+    }
+
+    fileprivate func loadUserPhotos() {
+        guard let imageUrl = user?.imageUrl1, let url = URL(string: imageUrl) else { return }
+        //SDWebImageManager caches and loads from the same url means it doesnt have to dl..
+        SDWebImageManager.shared.loadImage(with: url, options: .continueInBackground, progress: nil) { (image, _, _, _, _, _) in
+            self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
     }
 
     lazy var header: UIView = { //lazy var means the header is created after certain elements
@@ -115,18 +144,37 @@ class SettingsTableViewController: UITableViewController, UIImagePickerControlle
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = SettingsTableViewCell(style: .default, reuseIdentifier: nil)
 
-
         switch indexPath.section {
         case 1:
             cell.textField.placeholder = "Enter Name"
+            cell.textField.text = user?.name
+            cell.textField.addTarget(self, action: #selector(handleNameChange), for: .editingChanged)
         case 2:
             cell.textField.placeholder = "Enter Profession"
+            cell.textField.text = user?.profession
+            cell.textField.addTarget(self, action: #selector(handleProfessionChange), for: .editingChanged)
         case 3:
             cell.textField.placeholder = "Enter Age"
+            if let age = user?.age {
+                cell.textField.text = String(age)
+            }
+            cell.textField.addTarget(self, action: #selector(handleAgeChange), for: .editingChanged)
         default:
             cell.textField.placeholder = "Enter Bio"
         }
         return cell
+    }
+
+    @objc fileprivate func handleNameChange(textField: UITextField) {
+        self.user?.name = textField.text
+    }
+
+    @objc fileprivate func handleProfessionChange(textField: UITextField) {
+        self.user?.profession = textField.text
+    }
+
+    @objc fileprivate func handleAgeChange(textField: UITextField) {
+        self.user?.age = Int(textField.text ?? "")
     }
 
     fileprivate func setupNavigationItems() {
@@ -134,12 +182,34 @@ class SettingsTableViewController: UITableViewController, UIImagePickerControlle
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
         navigationItem.rightBarButtonItems = [
-            UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleCancel)),
+            UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave)),
             UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleCancel))
         ]
     }
 
     @objc fileprivate func handleCancel() {
         dismiss(animated: true)
+    }
+
+    @objc fileprivate func handleSave() { //persist specific user to firestore
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let docData: [String: Any] = [
+            "uid": uid,
+            "fullName": user?.name ?? "",
+            "imageUrl1": user?.imageUrl1 ?? "",
+            "age": user?.age ?? -1,
+            "profession": user?.profession ?? ""
+        ]
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Saving settings"
+        hud.show(in: view)
+        Firestore.firestore().collection("users").document(uid).setData(docData) { (err) in
+            hud.dismiss()
+            if let err = err {
+                print("Failed to save user settings:", err)
+                return
+            }
+            print("Finished saving user info")
+        }
     }
 }
